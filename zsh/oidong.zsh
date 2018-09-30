840ba6f79c58a0e272ab626e0bcc74d2959a8347
oidong <bremswind@web.de>
2018-09-30 17:43:14 +0200

    neue funktionen und aliase

--- zsh/oidong.zsh
+++ zsh/oidong.zsh
@@ -6,6 +6,9 @@ alias lrt='ls -lrt'
 ## Colorize the diff command output (colordiff package needed)
 alias diff='colordiff'
 
+## Colorize aurman 
+alias aurman='aurman --color always'
+
 ## Shortcut for tail -f
 alias tailf='tail -f'
 
@@ -23,4 +26,27 @@ function genpasswd() {
    l=40
   fi
   tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
+}
+
+function man() {
+        env \
+                LESS_TERMCAP_md=$(printf "\e[1;36m") \
+                LESS_TERMCAP_me=$(printf "\e[0m") \
+                LESS_TERMCAP_se=$(printf "\e[0m") \
+                LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+                LESS_TERMCAP_ue=$(printf "\e[0m") \
+                LESS_TERMCAP_us=$(printf "\e[1;32m") \
+                man "$@"
+}
+
+function yamllint() {
+        for i in $(find . -name '*.yml' -o -name '*.yaml'); do echo $i; ruby -e "require 'yaml';YAML.load_file(\"$i\")"; done
+}
+
+## Display Files without '#'-Comments
+alias uncomment='egrep -v "^\s*(#|$)"'
+
+## Using uncomment compined with grep
+function cgrep() {
+  uncomment $2 | egrep $1
 }
